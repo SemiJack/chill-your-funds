f856dd1c3d3dc3a0965a4d54de5d8ddbfbc5f3d4
erykkowalewicz <01168936@pw.edu.pl>
2022-06-08 20:47:01 +0200

    Logowanie działa!!!!!!!!!!!!!!!!!!!!!!!

--- src/main/java/chillyourfunds/client/LoginController.java
+++ src/main/java/chillyourfunds/client/LoginController.java
@@ -1,7 +1,33 @@
 package chillyourfunds.client;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+import java.awt.*;
 
 public class LoginController {
+    CYFClientController client=new CYFClientController("localhost","40000");
+    @FXML
+    TextField username=new TextField();
+    @FXML
+    PasswordField password=new PasswordField();
+    @FXML
+    Button okbutton=new Button();
+
+
+
+    String usernameString,passwordString;
+
+    public LoginController() throws Exception {
+    }
+
+    public void submit(ActionEvent e){
+        usernameString=username.getText();
+        passwordString=password.getText();
+        client.login(usernameString,passwordString);
+    }
 
 }
